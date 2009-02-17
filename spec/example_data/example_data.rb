c6c90bcd2fb1799518c6d6d9d0b717b5335e3377
Patrick Ewing <patrickhenryewing@gmail.com>
2009-02-17 01:09:39 -0800

    Updating JSON sample data with new methods

--- spec/example_data/example_data.rb
+++ spec/example_data/example_data.rb
@@ -12,46 +12,64 @@ end
 
 def limited_legislator_attributes
   <<-JSON
-    {"id":"B000444",
-    "name":"Joseph Biden",
-    "party":"D",
-    "state":"DE",
-    "district":"N/A"}
+   {
+    "id": "L000304",
+    "name": "Joseph I. Lieberman",
+    "first_name": "Joseph I.",
+    "middle_name": "I.",
+    "last_name": "Lieberman",
+    "party": "ID",
+    "state": "CT",
+                     "missed_votes_pct": "0.00",
+    "votes_with_party_pct": "93.44" 
+    }
   JSON
 end
 
 def full_legislator_attributes
   <<-JSON
   {
-     "member_id":"B000444",
-     "name":"Joseph Biden",
-     "date_of_birth":"1942-11-20",
-     "gender":"M",
-     "url":"http://biden.senate.gov",
-     "govtrack_id":"300008",
-     "roles":[
+     "status":"OK",
+     "copyright":"Copyright (c) 2009 The New York Times Company.  All Rights Reserved.",
+     "results":[
         {
-           "congress":"111",
-           "chamber":"Senate",
-           "title":"Senator, 2nd Class",
-           "state":"DE",
-           "party":"D",
-           "district":"N/A",
-           "start_date":"2009-01-06",
-           "end_date":"2009-01-20"
-        },
-        {
-           "congress":"110",
-           "chamber":"Senate",
-           "title":"Senator, 2nd Class",
-           "state":"DE",
-           "party":"D",
-           "district":"N/A",
-           "start_date":"2007-01-04",
-           "end_date":"2009-01-03"
+           "member_id": "L000304",
+           "name": "Joseph I. Lieberman",
+           "first_name": "Joseph I.",
+           "middle_name": "I.",
+           "last_name": "Lieberman",
+           "date_of_birth": "1942-02-24",
+           "gender": "M",
+           "url": "http://lieberman.senate.gov/",
+           "govtrack_id": "300067",
+           "roles": [
+           {
+             "congress": "111",
+             "chamber": "Senate",
+             "title": "Senator, 1st Class",
+             "state": "CT",
+             "party": "ID",
+             "district": "N/A",
+             "start_date": "2009-01-06",
+             "end_date": "2011-01-04",
+             "missed_votes_pct": "0.00",
+             "votes_with_party_pct": "93.44"
+           },        {
+             "congress": "110",
+             "chamber": "Senate",
+             "title": "Senator, 1st Class",
+             "state": "CT",
+             "party": "ID",
+             "district": "N/A",
+             "start_date": "2007-01-04",
+             "end_date": "2009-01-03",
+             "missed_votes_pct": "3.20",
+             "votes_with_party_pct": "86.79"
+           }
+          ]
         }
-     ]
-   }
+      ]
+    }
   JSON
 end
 
