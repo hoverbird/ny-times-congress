47c10cd8bcd57443182f7da0345cdb75fa6d7c50
Patrick Ewing <patrickhenryewing@gmail.com>
2009-02-17 01:05:24 -0800

    Updating README with better setup instructions

--- bin/setup.rb
+++ bin/setup.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 include NYTimes::Congress
 
-if ENV['NYTIMES_CONGRESS_API_KEY']
-  NYTimes::Congress::Base.api_key = ENV['NYTIMES_CONGRESS_API_KEY']
+if ENV['NY_TIMES_CONGRESS_API_KEY']
+  NYTimes::Congress::Base.api_key = ENV['NY_TIMES_CONGRESS_API_KEY']
   
   Senate = Congress.new(111, :senate)
   House  = Congress.new(111, :house)
