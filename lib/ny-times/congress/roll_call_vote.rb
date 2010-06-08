1cabf39e026da4f7f06bcd94b23bdca96675e8bc
Derek Willis <dwillis@gmail.com>
2010-06-08 15:36:32 -0400

    added chamber and majority positions to roll call votes

--- lib/ny-times/congress/roll_call_vote.rb
+++ lib/ny-times/congress/roll_call_vote.rb
@@ -6,23 +6,23 @@ module NYTimes
 			ATTRIBUTE_MAP = { 
 			  :date_for       =>  [:date],
         :integer_for    =>  [:session_number, :congress, :roll_call],
-        :symbol_for     =>  [:chamber],
-        :string_for     =>  [:bill_number, :question, :vote_type, :time, :result, :description],
-        :positions_for  =>  [:positions]
+        :string_for     =>  [:bill_number, :question, :vote_type, :time, :result, :description, :chamber],
+        :positions_for  =>  [:positions],
       }    			  
-      attr_reader *ATTRIBUTE_MAP.values.flatten
+      attr_reader :democratic_majority_position, :republican_majority_position, *ATTRIBUTE_MAP.values.flatten
 
 			def initialize(args={})
   			transformed_values = self.transform(args, ATTRIBUTE_MAP)
 				transformed_values.each_pair do |attribute, value|
 					instance_variable_set("@#{attribute}", value)
 				end
+				@democratic_majority_position, @republican_majority_position = string_for(args['democratic']['majority_position']), string_for(args['republican']['majority_position'])
 			end
 		
 		  def get_congress
 		    Congress.new(congress, chamber)
 		  end
-		  
+
 		end
 	end
 end
